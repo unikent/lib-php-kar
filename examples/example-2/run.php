973b9a1e485e8ed7dc7a4c40bdb6ee2cc5d46239
Skylar Kelty <S.Kelty@kent.ac.uk>
2015-02-05 15:01:16 +0000

    Fix example-2

--- examples/example-2/run.php
+++ examples/example-2/run.php
@@ -8,9 +8,13 @@
 
 require_once(dirname(__FILE__).'/../vendor/autoload.php');
 
+if (!isset($argv[1])) {
+    die("Usage: php run.php <author email>\n");
+}
+
 $api = new \unikent\KAR\API();
 
-$documents = $api->search_by_email($_GET['author']);
+$documents = $api->search_by_email($argv[1]);
 
 foreach ($documents as $document) {
     echo "--------------------------------- <br/>";
