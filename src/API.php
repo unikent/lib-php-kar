2443958d1d0138d3ad118bbbfa015089a4ac7cae
Skylar Kelty <S.Kelty@kent.ac.uk>
2016-08-02 09:56:17 -0400

    Applied fixes from StyleCI

--- src/API.php
+++ src/API.php
@@ -5,7 +5,6 @@
  * @copyright  2014 Skylar Kelty <S.Kelty@kent.ac.uk>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
 namespace unikent\KAR;
 
 /**
@@ -34,6 +33,7 @@ class API
      * CURL Timeout.
      *
      * @internal
+     *
      * @var int
      */
     private $_timeout;
@@ -42,6 +42,7 @@ class API
      * CURL Proxy.
      *
      * @internal
+     *
      * @var string
      */
     private $_proxy;
@@ -50,6 +51,7 @@ class API
      * URL of KAR.
      *
      * @internal
+     *
      * @var string
      */
     private $_url;
@@ -58,6 +60,7 @@ class API
      * A Static Internal Cache Layer.
      *
      * @internal
+     *
      * @var mixed
      */
     private $_internal_cache;
@@ -66,6 +69,7 @@ class API
      * A Cache Layer.
      *
      * @internal
+     *
      * @var mixed
      */
     private $_cache;
@@ -75,7 +79,8 @@ class API
      *
      * @param string $url Which KAR installation do you want? (just say null)
      */
-    public function __construct($url = null) {
+    public function __construct($url = null)
+    {
         $this->set_url($url);
         $this->set_timeout(0);
         $this->_cache = new StaticCache();
@@ -87,7 +92,8 @@ class API
      *
      * @param string $url Which KAR installation do you want? (just say null)
      */
-    public function set_url($url = null) {
+    public function set_url($url = null)
+    {
         if ($url === null) {
             $url = static::LIVE_URL;
         }
@@ -98,41 +104,45 @@ class API
     /**
      * Returns the URL.
      */
-    public function get_url() {
+    public function get_url()
+    {
         return $this->_url;
     }
 
     /**
-     * Set a custom CURL timeout
+     * Set a custom CURL timeout.
      *
      * @param int $timeout CURL Timeout in ms
      */
-    public function set_timeout($timeout) {
+    public function set_timeout($timeout)
+    {
         $this->_timeout = $timeout;
     }
 
     /**
-     * Set a custom CURL proxy
+     * Set a custom CURL proxy.
      *
      * @param string $proxy CURL proxy url
      */
-    public function set_proxy($proxy) {
+    public function set_proxy($proxy)
+    {
         $this->_proxy = $proxy;
     }
 
     /**
      * Cache people for eprintids.
      */
-    private function cache_people($ids) {
-        $ids = urlencode(join(',', $ids));
+    private function cache_people($ids)
+    {
+        $ids = urlencode(implode(',', $ids));
         $json = $this->curl($this->_url . "/cgi/api/get_people?q=$ids");
         $data = json_decode($json);
         if (!$data) {
             return;
         }
 
-        foreach ((array)$data as $eprintid => $people) {
-            usort($people, function($a, $b) {
+        foreach ((array) $data as $eprintid => $people) {
+            usort($people, function ($a, $b) {
                 return $a->id > $b->id;
             });
 
@@ -143,15 +153,16 @@ class API
     /**
      * Cache divisions for eprintids.
      */
-    private function cache_divisions($ids) {
-        $ids = urlencode(join(',', $ids));
+    private function cache_divisions($ids)
+    {
+        $ids = urlencode(implode(',', $ids));
         $json = $this->curl($this->_url . "/cgi/api/get_divisions?q=$ids");
         $data = json_decode($json);
         if (!$data) {
             return;
         }
 
-        foreach ((array)$data as $eprintid => $divisions) {
+        foreach ((array) $data as $eprintid => $divisions) {
             $this->_internal_cache->set($eprintid . '_divisions', $divisions);
         }
     }
@@ -159,15 +170,16 @@ class API
     /**
      * Cache files for eprintids.
      */
-    private function cache_files($ids) {
-        $ids = urlencode(join(',', $ids));
+    private function cache_files($ids)
+    {
+        $ids = urlencode(implode(',', $ids));
         $json = $this->curl($this->_url . "/cgi/api/get_files?q=$ids");
         $data = json_decode($json);
         if (!$data) {
             return;
         }
 
-        foreach ((array)$data as $eprintid => $files) {
+        foreach ((array) $data as $eprintid => $files) {
             $this->_internal_cache->set($eprintid . '_files', $files);
         }
     }
@@ -175,17 +187,18 @@ class API
     /**
      * Search KAR for a given x.
      *
-     * @param string $url The URL to grab.
-     * @param int $limit The maximum number of results to return.
-     * @param int $offset The offset of results to return.
+     * @param string $url    The URL to grab.
+     * @param int    $limit  The maximum number of results to return.
+     * @param int    $offset The offset of results to return.
      */
-    private function search_by_url($url, $limit = 1000, $offset = 0) {
-        $query = "&limit=" . urlencode($limit) . "&offset=" . urlencode($offset);
+    private function search_by_url($url, $limit = 1000, $offset = 0)
+    {
+        $query = '&limit=' . urlencode($limit) . '&offset=' . urlencode($offset);
         $json = $this->curl($this->_url . $url . $query);
         $objects = json_decode($json);
 
         if (!is_array($objects)) {
-            return null;
+            return;
         }
 
         $ids = array();
@@ -207,42 +220,47 @@ class API
      * Search KAR for a given eprintid.
      *
      * @param string $eprintid The eprint id.
-     * @param int $limit The maximum number of results to return.
-     * @param int $offset The offset of results to return.
+     * @param int    $limit    The maximum number of results to return.
+     * @param int    $offset   The offset of results to return.
      */
-    public function search_by_id($eprintid, $limit = 1000, $offset = 0) {
-        return $this->search_by_url("/cgi/api/search_by_id?q=" . urlencode($eprintid), $limit, $offset);
+    public function search_by_id($eprintid, $limit = 1000, $offset = 0)
+    {
+        return $this->search_by_url('/cgi/api/search_by_id?q=' . urlencode($eprintid), $limit, $offset);
     }
 
     /**
      * Search KAR for a given division.
      *
      * @param string $division The division id.
-     * @param int $limit The maximum number of results to return.
-     * @param int $offset The offset of results to return.
+     * @param int    $limit    The maximum number of results to return.
+     * @param int    $offset   The offset of results to return.
      */
-    public function search_by_division($division, $limit = 1000, $offset = 0) {
-        return $this->search_by_url("/cgi/api/search_by_division?q=" . urlencode($division), $limit, $offset);
+    public function search_by_division($division, $limit = 1000, $offset = 0)
+    {
+        return $this->search_by_url('/cgi/api/search_by_division?q=' . urlencode($division), $limit, $offset);
     }
 
     /**
      * Search KAR for a given author's email.
      *
-     * @param string $email The author's email.
-     * @param int $limit The maximum number of results to return.
-     * @param int $offset The offset of results to return.
+     * @param string $email  The author's email.
+     * @param int    $limit  The maximum number of results to return.
+     * @param int    $offset The offset of results to return.
      */
-    public function search_by_email($email, $limit = 1000, $offset = 0) {
-        return $this->search_by_url("/cgi/api/search_by_email?q=" . urlencode($email), $limit, $offset);
+    public function search_by_email($email, $limit = 1000, $offset = 0)
+    {
+        return $this->search_by_url('/cgi/api/search_by_email?q=' . urlencode($email), $limit, $offset);
     }
 
     /**
      * Return all people associated with a publication.
      *
      * @internal
+     *
      * @param string $eprintid The eprint id.
      */
-    public function get_people($eprintid) {
+    public function get_people($eprintid)
+    {
         if (!isset($this->_internal_cache->{$eprintid . '_people'})) {
             $this->cache_people(array($eprintid));
         }
@@ -251,7 +269,7 @@ class API
 
         $data = $this->_internal_cache->get($eprintid . '_people');
         if (!is_array($data)) {
-            return null;
+            return;
         }
 
         foreach ($data as $k => $v) {
@@ -270,16 +288,18 @@ class API
      * Return all people associated with a publication.
      *
      * @internal
+     *
      * @param string $eprintid The eprint id.
      */
-    public function get_divisions($eprintid) {
+    public function get_divisions($eprintid)
+    {
         if (!isset($this->_internal_cache->{$eprintid . '_divisions'})) {
             $this->cache_divisions(array($eprintid));
         }
 
         $data = $this->_internal_cache->get($eprintid . '_divisions');
         if (!is_array($data)) {
-            return null;
+            return;
         }
 
         return Division::create_paths_from_api($this, $data);
@@ -289,16 +309,18 @@ class API
      * Return all files associated with a publication.
      *
      * @internal
+     *
      * @param string $eprintid The eprint id.
      */
-    public function get_files($eprintid) {
+    public function get_files($eprintid)
+    {
         if (!isset($this->_internal_cache->{$eprintid . '_files'})) {
             $this->cache_files(array($eprintid));
         }
 
         $data = $this->_internal_cache->get($eprintid . '_files');
         if (!is_array($data)) {
-            return null;
+            return;
         }
 
         $files = array();
@@ -314,19 +336,22 @@ class API
      *
      * @param string $email The person's email address.
      */
-    public function get_person_url($email) {
+    public function get_person_url($email)
+    {
         $email = strtolower($email);
-        return $this->get_url() . "/view/email/" . $this->encode_string($email) . ".html";
+
+        return $this->get_url() . '/view/email/' . $this->encode_string($email) . '.html';
     }
 
     /**
      * Returns the URL for a file.
      *
      * @param string $eprintid The eprintid.
-     * @param string $pos The file position.
+     * @param string $pos      The file position.
      * @param string $filename The filename.
      */
-    public function get_file_url($eprintid, $pos, $filename) {
+    public function get_file_url($eprintid, $pos, $filename)
+    {
         return $this->get_url() . "/{$eprintid}/{$pos}/" . $filename;
     }
 
@@ -334,9 +359,11 @@ class API
      * Encode a string in EPrints URL format.
      *
      * @internal
+     *
      * @param string $string The string to encode.
      */
-    public function encode_string($string) {
+    public function encode_string($string)
+    {
         $string = rawurlencode($string);
         $string = str_replace('%', '=', $string);
         $string = str_replace('.', '=2E', $string);
@@ -350,9 +377,10 @@ class API
      *
      * @param object $cache An object with get and set methods.
      */
-    public function set_cache_layer($cache) {
+    public function set_cache_layer($cache)
+    {
         if (!method_exists($cache, 'set') || !method_exists($cache, 'get')) {
-            throw new \Exception("Invalid cache layer - must have set and get.");
+            throw new \Exception('Invalid cache layer - must have set and get.');
         }
 
         $this->_cache = $cache;
@@ -362,9 +390,11 @@ class API
      * CURL shorthand.
      *
      * @internal
+     *
      * @param string $url The URL to curl.
      */
-    protected function curl($url) {
+    protected function curl($url)
+    {
         if ($this->_cache !== null) {
             $v = $this->_cache->get($url);
             if ($v) {
@@ -373,11 +403,11 @@ class API
         }
 
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL,            $url);
+        curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HEADER,         false);
-        curl_setopt($ch, CURLOPT_HTTP_VERSION,   CURL_HTTP_VERSION_1_1);
-        curl_setopt($ch, CURLOPT_HTTPHEADER,     array('Content-Type: text/plain; charset=UTF-8'));
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: text/plain; charset=UTF-8'));
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
         if ($this->_timeout > 0) {
